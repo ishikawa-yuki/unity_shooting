31e47c2e9488d0de88cb11c7cd12da59d5a6ebc1
moriyama <kenshumoriyama01@gmail.com>
2019-06-19 12:06:42 +0900

    エラー修正

--- Assets/NormalEnemyController.cs
+++ Assets/NormalEnemyController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class NormalEnemyController : MonoBehaviour {
 
 	public GameObject EnemyBulletPrefab;
-	float delta = 0;		float moveSpan = 0.5f;
+	float delta = 0;
+	float moveSpan = 0.5f;
 	float moveDelta = 0;
 	float shootSpan = 1.5f;
 	float shootDelta = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -17,12 +19,15 @@ public class NormalEnemyController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		moveDelta += Time.deltaTime;
-		if(span < delta){			shootDelta +=Time.deltaTime;
-			delta = 0;			if(moveSpan < moveDelta){
-			moveDelta = 0;
-			transform.Translate(0, -0.3f, 0);
+		if(moveSpan < moveDelta){
+			shootDelta +=Time.deltaTime;
+			delta = 0;
+			if(moveSpan < moveDelta){
+				moveDelta = 0;
+				transform.Translate(0, -0.3f, 0);
+			}
 		}
-
+		shootDelta += Time.deltaTime;
 		if(shootSpan < shootDelta){
 			shootDelta = 0;
 			Instantiate (EnemyBulletPrefab, transform.position, Quaternion.identity);
