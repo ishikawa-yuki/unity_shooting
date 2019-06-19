38e26be26f960ddee370800b2047764b6676b57c
石河優貴 <burle42820@gmail.com>
2019-06-19 10:58:44 +0900

    BulletControllerの作成・PlayerControllerでの発射が可能に

---
+++ Assets/scripts/PlayerBulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0,0.3f,0);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log(other.gameObject.tag + "に当たりました");
+		if (other.gameObject.tag == "bar"){
+			Destroy(gameObject);
+		}	
+	}
+}
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	Rigidbody2D rigid2d;
+	public GameObject playerbulletPrefab;
+	public float deltime = 0;
+
+	float span = 0.1f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -24,6 +29,14 @@ public class PlayerController : MonoBehaviour {
 		}
 		if (Input.GetKey(KeyCode.RightArrow)){
 			transform.Translate(0,0.1f,0);
+		}
+		if (Input.GetKey(KeyCode.Space)){
+			this.deltime += Time.deltaTime;
+			if (this.deltime >= this.span ){
+				this.deltime = 0;
+				GameObject bullet = Instantiate(playerbulletPrefab) as GameObject;
+				bullet.transform.position = new Vector3(transform.position.x + 1.0f,transform.position.y,transform.position.z);	
+			}
 		}
 	}
 }
