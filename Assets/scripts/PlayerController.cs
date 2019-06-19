88c27607ec065ed2e492d34c1198999b1c2519ee
石河優貴 <burle42820@gmail.com>
2019-06-19 10:18:23 +0900

    PlayerControllerに移動を追加

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	Rigidbody2D rigid2d;
+
 	// Use this for initialization
 	void Start () {
-		
+		rigid2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey(KeyCode.UpArrow)){
+			transform.Translate(-0.1f,0,0);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			transform.Translate(0.1f,0,0);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate(0,-0.1f,0);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate(0,0.1f,0);
+		}
 	}
 }
