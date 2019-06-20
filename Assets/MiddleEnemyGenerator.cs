4827bbd7043aeb7c841796e709a68f046828ec6f
石河優貴 <burle42820@gmail.com>
2019-06-20 16:06:38 +0900

    Generatorの更新

--- Assets/MiddleEnemyGenerator.cs
+++ Assets/MiddleEnemyGenerator.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class MiddleEnemyGenerator : MonoBehaviour {
 
 	public GameObject MiddleEnemyPrefab;
-	float span = 10.0f;
+	GameObject director;
+	float span = 0;
 	float delta = 0;
 	float enemyPosy = 4.0f;
 
 	// Use this for initialization
 	void Start () {
-		
+		this.director = GameObject.Find("GameDirector");
+		int stageNum = this.director.GetComponent<GameDirector>().steage;
+		switch (stageNum) {
+			case 1:
+				this.span = 15.0f;
+				break;
+			case 2:
+				this.span = 12.0f;
+				break;
+			case 3:
+				this.span = 10.0f;
+				break;
+			default:
+				break;
+		}
 	}
 	
 	// Update is called once per frame
