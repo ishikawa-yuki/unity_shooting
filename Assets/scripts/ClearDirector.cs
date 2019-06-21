18c9d32fe7d47b77f599d4cb11f05f0b03c6c75a
石河優貴 <burle42820@gmail.com>
2019-06-21 10:19:22 +0900

    ClearScene作成

---
+++ Assets/scripts/ClearDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClearDirector : MonoBehaviour {
+
+	int score = 0;
+
+	int stock = 0;
+
+	GameObject clearText;
+
+	// Use this for initialization
+	void Start () {
+		this.clearText = GameObject.Find("clear");
+		this.stock = PlayerPrefs.GetInt("stockdata");
+		this.score = PlayerPrefs.GetInt("scoredata");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.clearText.GetComponent<Text>().text = this.score + "X" + this.stock + "\nSCORE : " + this.score*this.stock + "\nEnterキーでタイトルへ";
+		if (Input.GetKeyDown(KeyCode.Return)){
+			SceneManager.LoadScene("TitleScene");
+		}
+	}
+}
