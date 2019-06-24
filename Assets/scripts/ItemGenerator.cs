2edfc34d8598588c86c452ede11f05c85c4633df
石河優貴 <burle42820@gmail.com>
2019-06-24 13:40:39 +0900

    バリア機能の追加とそれに応じたbarrierコントローラーなどの追加・プレイヤーの弾発射部分の調整
    スペース短押しでも発射できるように変更

--- Assets/scripts/ItemGenerator.cs
+++ Assets/scripts/ItemGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ItemGenerator : MonoBehaviour {
 
 	public GameObject itemPrefab;
+	public GameObject barrieritemPrefab;
 	public float deltime = 0;
 	float span = 5.0f;
 
@@ -15,10 +16,16 @@ public class ItemGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		GameObject item;
 		this.deltime += Time.deltaTime;
 		if (this.deltime >= this.span){
 			this.deltime = 0;
-			GameObject item = Instantiate(itemPrefab) as GameObject;
+			int item_drop = Random.Range(4,-4);
+			if(item_drop % 2 == 0){
+				item = Instantiate(itemPrefab) as GameObject;
+			}else{
+				item = Instantiate(barrieritemPrefab) as GameObject;
+			}
 			int py = Random.Range(4,-4);
 			item.transform.position = new Vector3(8, py, 0);
 		}
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,10 +6,12 @@ public class PlayerController : MonoBehaviour {
 
 	public GameObject playerbulletPrefab;
 	public GameObject bombPrefab;
+	public GameObject barrierPrefab;
 	GameObject gamedirector;
 	public float deltime = 0;
 	float span = 0.2f;
 	public int bullet_type = 0;
+	bool barrier_flag = false;
 
 
 	// Use this for initialization
@@ -40,8 +42,8 @@ public class PlayerController : MonoBehaviour {
 				transform.Translate(0,0.1f,0);
 			}
 		}
+		this.deltime += Time.deltaTime;
 		if (Input.GetKey(KeyCode.Space)){
-			this.deltime += Time.deltaTime;
 			if (this.deltime >= this.span ){
 				this.deltime = 0;
 				switch (this.bullet_type){
@@ -54,7 +56,6 @@ public class PlayerController : MonoBehaviour {
 					 break;
 
 					default:
-					Debug.Log("kuso");
 					GameObject bullet = Instantiate(playerbulletPrefab) as GameObject;
 					bullet.transform.position = new Vector3(transform.position.x + 1.0f,transform.position.y,transform.position.z);
 					break;
@@ -62,7 +63,6 @@ public class PlayerController : MonoBehaviour {
 
 			}
 		}
-
 		if (Input.GetKeyDown(KeyCode.B)){
 			if (this.gamedirector.GetComponent<GameDirector>().bombstock >= 1){
 				GameObject bomb = Instantiate(bombPrefab) as GameObject;
@@ -78,11 +78,27 @@ public class PlayerController : MonoBehaviour {
 			Destroy(other.gameObject);
 			this.bullet_type = 1;
 		}
-		if (other.gameObject.tag == "enemybullet" || other.gameObject.tag == "enemy" || other.gameObject.tag == "boss"){
-			Destroy(gameObject);
+		if (other.gameObject.tag == "barrieritem"){
 			Destroy(other.gameObject);
-			PlayerPrefs.SetInt("bullet_mode",0);
-			this.gamedirector.GetComponent<GameDirector>().player_kill();
+			Debug.Log(this.barrier_flag);
+			if (!(this.barrier_flag)){
+				GameObject barrier = Instantiate(barrierPrefab) as GameObject;
+				barrier.transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z);
+				this.barrier_flag = true;	
+			}
+		}
+		if (other.gameObject.tag == "enemybullet" || other.gameObject.tag == "enemy" || other.gameObject.tag == "boss" || other.gameObject.tag == "obstacle"){
+			if (!(other.gameObject.tag == "obstacle")){
+				Destroy(other.gameObject);
+			}
+			if (this.barrier_flag){
+				Destroy(GameObject.FindGameObjectWithTag("barrier"));
+				this.barrier_flag = false;
+			}else{
+				Destroy(gameObject);
+				PlayerPrefs.SetInt("bullet_mode",0);
+				this.gamedirector.GetComponent<GameDirector>().player_kill();
+			}
 		}
 	}
 }
