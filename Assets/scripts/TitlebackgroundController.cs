f63b2f873ee65da38c6878a751b5ec1bae41ab27
石河優貴 <burle42820@gmail.com>
2019-06-25 09:30:41 +0900

    ポーズ機能追加のための修正

--- Assets/scripts/TitlebackgroundController.cs
+++ Assets/scripts/TitlebackgroundController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundController : MonoBehaviour {
+public class TitleBackgroundController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
